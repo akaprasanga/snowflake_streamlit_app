03639d5b7e8dc58a60f1151381de635d36a3df46
akaprasanga <071bct525prasanga@pcampus.edu.np>
2023-06-27 09:19:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-print("hello")
+import streamlit
+
+streamlit.title("My parents New Healthy Diner ")
+streamlit.text("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard Boiled Free Range egg")
